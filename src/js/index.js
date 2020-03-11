e2a153eca9b9dce9bfbcd08fe52a7f6cb758559e
Zach <zach@secondcloset.com>
2020-03-11 16:07:56 -0400

    Fixed up authorization to use the service

--- src/js/index.js
+++ src/js/index.js
@@ -4,106 +4,107 @@ let heatmapData = [];
 let visibleMap = false;
 let map, westernMarker, dataUrl, marker, latLng, heatmap;
 
-function initMap() {
-    var styledMapType = new google.maps.StyledMapType([
-        {
-            "elementType": "geometry",
-            "stylers": [{ "color": "#212121" }]
-        },
-        {
-            "elementType": "labels.icon",
-            "stylers": [{ "visibility": "off" }]
-        },
-        {
-            "elementType": "labels.text.fill",
-            "stylers": [{ "color": "#757575" }]
-        },
-        {
-            "elementType": "labels.text.stroke",
-            "stylers": [{ "color": "#212121" }]
-        },
-        {
-            "featureType": "administrative",
-            "elementType": "geometry",
-            "stylers": [{ "color": "#757575" }]
-        },
-        {
-            "featureType": "administrative.country",
-            "elementType": "labels.text.fill",
-            "stylers": [{ "color": "#9e9e9e" }]
-        },
-        {
-            "featureType": "administrative.locality",
-            "elementType": "labels.text.fill",
-            "stylers": [{ "color": "#bdbdbd" }]
-        },
-        {
-            "featureType": "poi",
-            "elementType": "labels.text.fill",
-            "stylers": [{ "color": "#757575" }]
-        },
-        {
-            "featureType": "poi.park",
-            "elementType": "geometry",
-            "stylers": [{ "color": "#181818" }]
-        },
-        {
-            "featureType": "poi.park",
-            "elementType": "labels.text.fill",
-            "stylers": [{ "color": "#616161" }]
-        },
-        {
-            "featureType": "poi.park",
-            "elementType": "labels.text.stroke",
-            "stylers": [{ "color": "#1b1b1b" }]
-        },
-        {
-            "featureType": "road",
-            "elementType": "geometry.fill",
-            "stylers": [{ "color": "#2c2c2c" }]
-        },
-        {
-            "featureType": "road",
-            "elementType": "labels.text.fill",
-            "stylers": [{ "color": "#8a8a8a" }]
-        },
-        {
-            "featureType": "road.arterial",
-            "elementType": "geometry",
-            "stylers": [{ "color": "#373737" }]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "geometry",
-            "stylers": [{ "color": "#3c3c3c" }]
-        },
-        {
-            "featureType": "road.highway.controlled_access",
-            "elementType": "geometry",
-            "stylers": [{ "color": "#4e4e4e" }]
-        },
-        {
-            "featureType": "road.local",
-            "elementType": "labels.text.fill",
-            "stylers": [{ "color": "#616161" }]
-        },
-        {
-            "featureType": "transit",
-            "elementType": "labels.text.fill",
-            "stylers": [{ "color": "#757575" }]
-        },
-        {
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [{ "color": "#000000" }]
-        },
-        {
-            "featureType": "water",
-            "elementType": "labels.text.fill",
-            "stylers": [{ "color": "#3d3d3d" }]
-        }
-    ]);
+let mapStyle = [
+    {
+        "elementType": "geometry",
+        "stylers": [{ "color": "#212121" }]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [{ "visibility": "off" }]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [{ "color": "#757575" }]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [{ "color": "#212121" }]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [{ "color": "#757575" }]
+    },
+    {
+        "featureType": "administrative.country",
+        "elementType": "labels.text.fill",
+        "stylers": [{ "color": "#9e9e9e" }]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [{ "color": "#bdbdbd" }]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [{ "color": "#757575" }]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [{ "color": "#181818" }]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [{ "color": "#616161" }]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels.text.stroke",
+        "stylers": [{ "color": "#1b1b1b" }]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [{ "color": "#2c2c2c" }]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [{ "color": "#8a8a8a" }]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [{ "color": "#373737" }]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [{ "color": "#3c3c3c" }]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry",
+        "stylers": [{ "color": "#4e4e4e" }]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [{ "color": "#616161" }]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [{ "color": "#757575" }]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{ "color": "#000000" }]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [{ "color": "#3d3d3d" }]
+    }
+];
 
+function initMap() {
+    var styledMapType = new google.maps.StyledMapType(mapStyle);
 
     map = new google.maps.Map(document.getElementById("map"), {
         center: westernLatLong,
@@ -126,14 +127,18 @@ function initMap() {
 
 
 function toggleMap() {
-    let mapElement = document.getElementById("map");
-    visibleMap = !visibleMap;
+    var auth2 = gapi.auth2.getAuthInstance();
 
-    if(visibleMap) {
-        mapElement.classList.add("show");
-    }
-    else {
-        mapElement.classList.remove("show");
+    if(auth2.isSignedIn.je) {
+        let mapElement = document.getElementById("map");
+        visibleMap = !visibleMap;
+
+        if(visibleMap) {
+            mapElement.classList.add("show");
+        }
+        else {
+            mapElement.classList.remove("show");
+        }
     }
 }
 
