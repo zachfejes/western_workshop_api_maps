32953bac7c54ab22489067a4fbe36d692b4d925f
Zach <zach@secondcloset.com>
2020-03-11 00:05:43 -0400

    updated map

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,122 @@
 let westernLatLong = { lat: 43.0081693, lng: -81.2766837 };
 let initialZoom = 19.76;
 let heatmapData = [];
+let visibleMap = false;
 let map, westernMarker, dataUrl, marker, latLng, heatmap;
 
 function initMap() {
+    var styledMapType = new google.maps.StyledMapType([
+        {
+            "elementType": "geometry",
+            "stylers": [{ "color": "#212121" }]
+        },
+        {
+            "elementType": "labels.icon",
+            "stylers": [{ "visibility": "off" }]
+        },
+        {
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#757575" }]
+        },
+        {
+            "elementType": "labels.text.stroke",
+            "stylers": [{ "color": "#212121" }]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#757575" }]
+        },
+        {
+            "featureType": "administrative.country",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#9e9e9e" }]
+        },
+        {
+            "featureType": "administrative.locality",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#bdbdbd" }]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#757575" }]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#181818" }]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#616161" }]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "labels.text.stroke",
+            "stylers": [{ "color": "#1b1b1b" }]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry.fill",
+            "stylers": [{ "color": "#2c2c2c" }]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#8a8a8a" }]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#373737" }]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#3c3c3c" }]
+        },
+        {
+            "featureType": "road.highway.controlled_access",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#4e4e4e" }]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#616161" }]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#757575" }]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{ "color": "#000000" }]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [{ "color": "#3d3d3d" }]
+        }
+    ]);
+
+
     map = new google.maps.Map(document.getElementById("map"), {
         center: westernLatLong,
-        zoom: initialZoom
+        zoom: initialZoom,
+        mapTypeControlOptions: {
+            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map']
+        }
     });
 
     westernMarker = new google.maps.Marker({position: westernLatLong, map: map});
+
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
     
     fetchMeteoriteData((error, response) => {
         renderHeatMap(response);
@@ -17,6 +124,18 @@ function initMap() {
     });
 };
 
+
+function toggleMap() {
+    let mapElement = document.getElementById("map");
+    visibleMap = !visibleMap;
+
+    if(visibleMap) {
+        mapElement.classList.add("show");
+    }
+    else {
+        mapElement.classList.remove("show");
+    }
+}
 
 
 function renderHeatMap(rawData) {
@@ -27,7 +146,7 @@ function renderHeatMap(rawData) {
 
         heatmapData.push({
             location: new google.maps.LatLng(geolocation.latitude, geolocation.longitude),
-            weight: Math.pow(mass, 0.2)
+            weight: Math.pow(mass, 0.1)
         });
     }
 
@@ -35,7 +154,9 @@ function renderHeatMap(rawData) {
         new google.maps.visualization.HeatmapLayer({
             data: heatmapData,
             dissipating: true,
-            map: map
+            map: map,
+            radius: 15,
+            opacity: 0.5
         })
     );
 }
